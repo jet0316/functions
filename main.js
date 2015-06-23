7e6b71b0b2ead2364100dc1b2e1962702dc6048a
Justin <jethielke@gmail.com>
2015-06-23 15:12:41 -0600

    functions

---
+++ main.js
@@ -0,0 +1,42 @@
+var trippleFive = function (){
+	for (var i = 0; i <3; i++) {
+		console.log('Five!');
+	};
+}
+ trippleFive()
+
+//============================================================
+
+ var lastLeter = function (word) {
+ 	var letter = word.charAt(word.length - 1)
+ 		return letter
+ }
+
+ console.log(lastLeter('rob'))
+
+ //=============================================================
+
+ var square = function (number) {
+ 	return number * number
+ }
+
+ console.log(square(10))
+
+ //==============================================================
+
+ var negate = function (number) {
+ 	return -number
+ }
+
+ console.log(negate(-35))
+
+ //============================================================
+
+var toArray = function (a, b, c){
+	var array = [a,b,c]
+	return array
+ }
+
+console.log(toArray(1,4,5))
+
+//=============================================================
